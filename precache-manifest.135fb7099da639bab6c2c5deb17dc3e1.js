f7dc69ca2ccb03cf5e9d0c22adbd5a2a09c87e3e
rmdza <dev.rdmza@gmail.com>
2019-06-24 15:32:34 +0800

    Updates

---
+++ precache-manifest.135fb7099da639bab6c2c5deb17dc3e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb295f4607cc239a2140a48aa525e34",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d99cd477a22dddfab436",
+    "url": "/react-tic-tac-toe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/react-tic-tac-toe/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "d99cd477a22dddfab436",
+    "url": "/react-tic-tac-toe/static/js/main.3f05796a.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  }
+]);
